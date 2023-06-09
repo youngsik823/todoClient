e9a22a31ed6fe6a73e8c7602f6f8a6f539437375
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-06-09 09:49:42 +0900

    #11 - 회원가입 입력값 검증 1

--- src/component/user/Join.js
+++ src/component/user/Join.js
@@ -1,6 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import {Button, Container, Grid,
-  TextField, Typography, Link} from "@mui/material";
+import React, {
+  useEffect,
+  useState
+} from 'react';
+import {
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+  Link
+} from "@mui/material";
 
 const Join = () => {
 
@@ -19,6 +28,40 @@ const Join = () => {
     email: ''
   });
 
+  // 검증 완료 체크에 대한 상태변수 관리
+  const [correct, setCorrect] = useState({
+    userName: false,
+    password: false,
+    passwordCheck: false,
+    email: false
+  });
+
+
+  // 검증데이터를 상태변수에 저장하는 함수
+  const saveInputState = ({
+    key,
+    inputVal,
+    flag,
+    msg
+  }) => {
+
+    inputVal !== 'pass' && setUserValue({
+      ...userValue,
+      [key]: inputVal
+    });
+
+    setMessage({
+      ...message,
+      [key]: msg
+    });
+
+
+    setCorrect({
+      ...correct,
+      [key]: flag
+    });
+  };
+
 
   // 이름 입력창 체인지 이벤트 핸들러
   const nameHandler = e => {
@@ -28,25 +71,26 @@ const Join = () => {
     const inputVal = e.target.value;
     // 입력값 검증
     let msg; // 검증 메시지를 저장할 변수
+    let flag; // 입력 검증체크 변수
 
     if (!inputVal) {
       msg = '유저 이름은 필수입니다.';
+      flag = false;
     } else if (!nameRegex.test(inputVal)) {
       msg = '2~5글자 사이의 한글로 작성하세요!';
+      flag = false;
     } else {
       msg = '사용 가능한 이름입니다.';
+      flag = true;
     }
 
-    setMessage({
-      ...message,
-      userName: msg
+    saveInputState({
+      key: 'userName',
+      inputVal,
+      msg,
+      flag
     });
 
-
-    setUserValue({
-      ...userValue,
-      userName: inputVal
-    });
 
   };
 
@@ -65,11 +109,59 @@ const Join = () => {
   // 패스워드 입력창 체인지 이벤트 핸들러
   const passwordHandler = e => {
 
+    // 패스워드가 변동되면 확인란을 비우기
+    document.getElementById('password-check').value = '';
+    document.getElementById('check-span').textContent = '';
+
+    setMessage({...message, passwordCheck: ''});
+    setCorrect({...correct, passwordCheck: false});
+
     const inputVal = e.target.value;
 
-    setUserValue({
-      ...userValue,
-      password: inputVal
+    const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/;
+
+    // 검증 시작
+    let msg, flag;
+    if (!e.target.value) { // 패스워드 안적은거
+      msg = '비밀번호는 필수값입니다!';
+      flag = false;
+    } else if (!pwRegex.test(e.target.value)) {
+      msg = '8글자 이상의 영문,숫자,특수문자를 포함해주세요!';
+      flag = false;
+    } else {
+      msg = '사용 가능한 비밀번호입니다.';
+      flag = true;
+    }
+
+    saveInputState({
+      key: 'password',
+      inputVal,
+      msg,
+      flag
+    });
+
+  };
+
+  // 비밀번호 확인란 검증 이벤트 핸들러
+  const pwCheckHandler = e => {
+    // 검증 시작
+    let msg, flag;
+    if (!e.target.value) { // 패스워드 안적은거
+      msg = '비밀번호 확인란은 필수값입니다!';
+      flag = false;
+    } else if (userValue.password !== e.target.value) {
+      msg = '패스워드가 일치하지 않습니다.';
+      flag = false;
+    } else {
+      msg = '패스워드가 일치합니다.';
+      flag = true;
+    }
+
+    saveInputState({
+      key: 'passwordCheck',
+      inputVal: 'pass',
+      msg,
+      flag
     });
 
   };
@@ -110,7 +202,12 @@ const Join = () => {
                         autoFocus
                         onChange={nameHandler}
                     />
-                    <span>{message.userName}</span>
+                    <span style={
+                      correct.userName
+                      ?{color:'green'}
+                      : {color:'red'}
+                    }>{message.userName}</span>
+                    {/* 문자열만 {}생략가능 다른 모든것은 {}써야한다. */}
                 </Grid>
                 <Grid item xs={12}>
                     <TextField
@@ -137,7 +234,11 @@ const Join = () => {
                         autoComplete="current-password"
                         onChange={passwordHandler}
                     />
-                    <span></span>
+                    <span style={
+                      correct.password
+                      ?{color:'green'}
+                      : {color:'red'}
+                    }>{message.password}</span>
                 </Grid>
 
                 <Grid item xs={12}>
@@ -150,9 +251,13 @@ const Join = () => {
                         type="password"
                         id="password-check"
                         autoComplete="check-password"
-                
+                        onChange={pwCheckHandler}                
                     />
-                    <span id="check-text"></span>
+                    <span id='check-span' style={
+                      correct.passwordCheck
+                      ?{color:'green'}
+                      : {color:'red'}
+                    }>{message.passwordCheck}</span>
                 </Grid>
 
                 <Grid item xs={12}>
@@ -177,6 +282,7 @@ const Join = () => {
         </form>
     </Container>
   );
+
 }
 
 export default Join
